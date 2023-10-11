17c002e0c9e00da4e27faccf12461a3ce9d6a12c
bishnulimbu <subashlimbu987654321@gmail.com>
2023-10-11 12:03:46 +0545

    pointer and address

--- One.c
+++ One.c
@@ -1,5 +1,12 @@
 //hellow world.
 #include <stdio.h>
 int main(){
-  printf("Output1");
+  // printf("Output1");
+  int x = 10;
+  int *y = &x;
+  printf("%d\n", x);
+  printf("%u\n", &x);
+  printf("%u\n", y);
+  printf("%d\n", *y);
+  printf("%u\n", &y);
 }
