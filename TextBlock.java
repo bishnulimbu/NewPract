96bfa944f15907bbabf192ad9fb1cb49d903c3a3
bishnulimbu <subashlimbu987654321@gmail.com>
2023-10-13 17:30:43 +0545

    asdfsdf

--- TextBlock.java
+++ TextBlock.java
@@ -1,26 +1,33 @@
-// import com.sun.tools.javac.util.Name;
+import java.util.Scanner;
+import java.util.regex.MatchResult;
 
-// public class TextBlock {
-//   public static void main(String[] args) {
-//     String NameBlock = """
-//     One tow
-//     three
-//     four five
-//     five
-//     one Two """;
-//     System.out.println(NameBlock);
-
-//   }
-// }
 public class TextBlock {
-
-    public static void main(String[] args) {
-        String textBlock = """
-            This is a text block.
-            It can span multiple lines
-            and preserves formatting.
-            """;
-
-        System.out.println(textBlock);
+  public static void main(String[] args) {
+    String NameBlock = """
+    One Two 
+    three
+    four five
+    five
+    Five
+    one Two """;
+    try(Scanner scan = new Scanner(NameBlock)){
+      scan.findAll("five").map(MatchResult::group).forEach(System.out::println);
     }
+  }
+}
+//this is a case sensative as Scanner in default is case-sensative in nature:
+// public class TextBlock {
+
+//     public static void main(String[] args) {
+//         String textBlock = """
+//             This is a text block.
+//             It can span multiple lines
+//             and preserves formatting.
+//             """;
+
+//         System.out.println(textBlock);
+//     }
+// }
+// scan.findAll("word to be found").map(MatchResult::group).for (sysouut ::ptrintln {
+  
 }
