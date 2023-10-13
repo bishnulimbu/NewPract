de02686644a9f77b66af52fd46de692f2d7ede8f
bishnulimbu <subashlimbu987654321@gmail.com>
2023-10-13 16:24:59 +0545

    dfasf

---
+++ TextBlock.java
@@ -0,0 +1,26 @@
+// import com.sun.tools.javac.util.Name;
+
+// public class TextBlock {
+//   public static void main(String[] args) {
+//     String NameBlock = """
+//     One tow
+//     three
+//     four five
+//     five
+//     one Two """;
+//     System.out.println(NameBlock);
+
+//   }
+// }
+public class TextBlock {
+
+    public static void main(String[] args) {
+        String textBlock = """
+            This is a text block.
+            It can span multiple lines
+            and preserves formatting.
+            """;
+
+        System.out.println(textBlock);
+    }
+}
