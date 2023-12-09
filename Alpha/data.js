f5b2257cb22862bcb49c269af567176360061a40
bishnulimbu <subashlimbu987654321@gmail.com>
2023-12-10 01:23:56 +0545

    js

---
+++ Alpha/data.js
@@ -0,0 +1,12 @@
+let x = "ram" + 10 + 10;
+let y = 16 + 4 + "Volvo";
+console.log(x);
+console.log(y);
+
+//object in js
+const person ={
+    name: "bishnu",
+    age: 21,
+    job: "programmer"
+};
+console.log(person.name+ ' ' +person.age+ ' ' + person.job);
