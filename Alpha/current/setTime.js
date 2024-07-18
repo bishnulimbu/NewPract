3e29adafbed37683df579bb89d51e3a84d896963
bishnulimbu <subashlimbu987654321@gmail.com>
2024-07-18 21:45:34 +0545

    setTimeinterval practice

---
+++ Alpha/current/setTime.js
@@ -0,0 +1,10 @@
+// const repeat = () => {
+//   console.log("hello");
+// };
+// setTimeout(() => {
+//   console.log("hello");
+// }, 2000);
+setInterval(() => {
+  console.log("hello");
+}, 2000);
+//interval will loop over and over again.
