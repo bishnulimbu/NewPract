321b8cf6888c0503d7bf2f3ed61450a3ae23c0d8
bishnulimbu <subashlimbu987654321@gmail.com>
2024-07-27 17:49:38 +0545

    refactored the code using promises

--- Alpha/current/callBackHell.js
+++ Alpha/current/callBackHell.js
@@ -1,19 +1,42 @@
 let text1 = document.querySelector("h1");
 
-function changeColor(color, delay, callFn1) {
-  setTimeout(() => {
-    text1.style.color = color;
-    if (callFn1) {
-      callFn1();
-    }
-  }, delay);
-}
-changeColor("red", 1000, () => {
-  changeColor("blue", 1000, () => {
-    changeColor("green", 1000, () => {
-      changeColor("orange", 1000);
-    });
-  });
-});
+// function changeColor(color, delay, callFn1) {
+//   setTimeout(() => {
+//     text1.style.color = color;
+//     if (callFn1) {
+//       callFn1();
+//     }
+//   }, delay);
+// }
+// changeColor("red", 1000, () => {
+//   changeColor("blue", 1000, () => {
+//     changeColor("green", 1000, () => {
+//       changeColor("orange", 1000);
+//     });
+//   });
+// });
 //callback hell demo
 //promises will be used for meditaing the problem
+
+function changeColor(color, delay) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      text1.style.color = color;
+      resolve("color changed");
+    }, delay);
+  });
+}
+
+changeColor("red", 1000)
+  .then(() => {
+    console.log("color changed");
+    changeColor("blue", 1000).then(() => {
+      console.log("blue color");
+      changeColor("green", 1000).then(() => {
+        console.log("green color");
+      });
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
